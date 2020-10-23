1a51c93ae744f89f0646affe3fdd7c00c8692f7d
Augustine Dickinson <augustine.dickinson@icloud.com>
2020-10-23 14:48:43 +0200

    Init

---
+++ index.js
@@ -0,0 +1,56 @@
+// Express //
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+app.use('/static', express.static('public'))
+
+// Handlebars //
+const hbs = require('express-handlebars')
+app.engine('.hbs', hbs({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Sass //
+const compileSass = require('express-compile-sass')
+const root = process.cwd()
+app.use(compileSass({
+  root: root
+}))
+app.use(express.static(root))
+
+// GitHub API //
+const { Octokit } = require("@octokit/rest")
+var GitHub = new Octokit()
+
+// Routes //
+app.get('/', function(req, res, next) {
+})
+
+app.get('/works/', function(req, res, next) {
+})
+
+app.get('/works/:id', function(req, res, next) {
+  const id = req.params.id
+  const clavis = parseInt(id.substring(3))
+  const dir = (Math.floor(clavis/1000) * 1000 + 1) + '-' + (Math.ceil(clavis/1000) * 1000)
+
+  GitHub.repos.getContents({
+    owner: 'BetaMasaheft',
+    repo: 'Works',
+    branch: 'master',
+    path: dir+'/'+id+'.xml'
+  })
+  .then(({ data }) => {
+    var fileSHA = data.sha
+
+    return GitHub.git.getBlob({
+      owner: owner,
+      repo: repo,
+      file_sha: fileSHA
+    })
+  })
+  .then(({ data }) => {
+    var xmlContent = new Buffer.from(data.content, 'base64')
+    xmlContent = xmlContent.toString()
+    res.send(xmlContent)
+  })
+})
