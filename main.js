859f9642e54845b94b0e25dffa6e086295c48350
Augustine Dickinson <augustine.dickinson@icloud.com>
2020-11-19 14:56:48 +0100

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 
+const ElectronPrefs = require('electron-prefs');
+const prefs = new ElectronPrefs({
+  fileName: "prefs"
+})
+
+const fs = require('fs')
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
@@ -9,15 +16,29 @@ function createWindow () {
     }
   })
 
-  win.loadFile('index.html')
+  if(prefs.get('masterDirectory') == undefined) win.loadFile('setup.html')
+  else {
+    fs.readdir(prefs.get('masterDirectory'), (err, files) => {
+      win.loadFile('index.html')
+      win.webContents.send('updateRepos', files)
+      console.log(files)
+    })
+  }
 
   ipcMain.on('openDirectory', function () {
 
     dialog.showOpenDialog(win, {
       properties: ['openDirectory']
     }).then(result => {
-      console.log(result.canceled)
-      console.log(result.filePaths)
+      if(result.canceled) console.log('cancelled')
+      else {
+        prefs.set('masterDirectory', result.filePaths[0])
+
+        fs.readdir(prefs.get('masterDirectory'), (err, files) => {
+          win.loadFile('index.html')
+          win.webContents.send('updateRepos', files)
+        })
+      }
     }).catch(err => {
       console.log(err)
     })
